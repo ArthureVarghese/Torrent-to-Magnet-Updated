f5985f60238c2a34955306e2b7b96bf0d47c7303
Arthure Antony Varghese <56860740+ArthureVarghese@users.noreply.github.com>
2021-08-23 22:42:15 +0530

    Include all tracker
    Updated code to include All trackers present in the torrent file

--- torrent2magnet.py
+++ torrent2magnet.py
@@ -2,9 +2,19 @@ import sys
 import bencodepy
 import hashlib
 import base64
-
+import re
+regex=r"\b((?:https?://)?(?:udp?://)?(?:tcp?://)?(?:(?:www\.)?(?:[\da-z\.-]+)\.(?:[a-z]{2,6})|(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)|(?:(?:[0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|(?:[0-9a-fA-F]{1,4}:){1,7}:|(?:[0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|(?:[0-9a-fA-F]{1,4}:){1,5}(?::[0-9a-fA-F]{1,4}){1,2}|(?:[0-9a-fA-F]{1,4}:){1,4}(?::[0-9a-fA-F]{1,4}){1,3}|(?:[0-9a-fA-F]{1,4}:){1,3}(?::[0-9a-fA-F]{1,4}){1,4}|(?:[0-9a-fA-F]{1,4}:){1,2}(?::[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:(?:(?::[0-9a-fA-F]{1,4}){1,6})|:(?:(?::[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(?::[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(?:ffff(?::0{1,4}){0,1}:){0,1}(?:(?:25[0-5]|(?:2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(?:25[0-5]|(?:2[0-4]|1{0,1}[0-9]){0,1}[0-9])|(?:[0-9a-fA-F]{1,4}:){1,4}:(?:(?:25[0-5]|(?:2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(?:25[0-5]|(?:2[0-4]|1{0,1}[0-9]){0,1}[0-9])))(?::[0-9]{1,4}|[1-5][0-9]{4}|6[0-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|6553[0-5])?(?:/[\w\.-]*)*/?)\b"
 def make_magnet_from_file(file) :
     metadata = bencodepy.decode_from_file(file)
+    num = len(metadata[b'announce-list'])
+    tr = 0
+    for i in range(num):
+        data = str(metadata[b'announce-list'][i])
+        link = re.findall(regex,data)
+        if(tr==0):
+            tr = '&tr=' + str(link[0])
+        else:
+            tr = tr + '&tr=' + str(link[0])
     subj = metadata[b'info']
     hashcontents = bencodepy.encode(subj)
     digest = hashlib.sha1(hashcontents).digest()
@@ -12,8 +22,7 @@ def make_magnet_from_file(file) :
     return 'magnet:?'\
              + 'xt=urn:btih:' + b32hash\
              + '&dn=' + metadata[b'info'][b'name'].decode()\
-             + '&tr=' + metadata[b'announce'].decode()\
-             + '&xl=' + str(metadata[b'info'][b'length'])
+             + tr
 
 if __name__ == "__main__":
     magnet = make_magnet_from_file(sys.argv[1])
